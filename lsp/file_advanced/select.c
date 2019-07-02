2d1507cb1718dbc9c2e89b0ec6f4518bef88904b
surinoel <48958763+surinoel@users.noreply.github.com>
2019-07-02 09:25:42 +0900

    Update select.c

--- lsp/file_advanced/select.c
+++ lsp/file_advanced/select.c
@@ -7,16 +7,6 @@
 #include <sys/select.h>
 #include <sys/inotify.h>
 
-#if 0
-	struct inotify_event {
-	   int      wd;       /* Watch descriptor */
-	   uint32_t mask;     /* Mask describing event */
-	   uint32_t cookie;   /* Unique cookie associating related
-							 events (for rename(2)) */
-	   uint32_t len;      /* Size of name field */
-	   char     name[];   /* Optional null-terminated name */
-	};
-#endif
 int main(int argc, char **argv)
 {
 	int fd = -1;
@@ -25,8 +15,8 @@ int main(int argc, char **argv)
 	char buf[1024];
 	int ret;
 	struct inotify_event *event;
-    fd_set fds;
-    struct timeval timeout;
+    	fd_set fds;
+    	struct timeval timeout;
 
 	fd = inotify_init();
 	if (fd == -1) {
@@ -44,51 +34,49 @@ int main(int argc, char **argv)
 		printf("add_watch() fail\n");
 		goto err;
 	}
-    while (1) {
-        FD_SET(fd, &fds);
-        FD_SET(STDIN_FILENO, &fds);
-        timeout.tv_sec = 5;
-        timeout.tv_usec = 0;
-        ret = select(fd > STDIN_FILENO ? fd + 1 : STDIN_FILENO + 1, &fds,
-            NULL, NULL, &timeout);
-        if(ret == -1) {
-            printf("select error\n");
-            goto err;
-        } else if(ret == 0) {
-            printf("timeout expire !\n");
-        } else if(ret > 0) {
-            if(FD_ISSET(fd, &fds)) {
-                memset(buf, 0, sizeof(buf));
-                ret = read(fd, buf, sizeof(buf));
-                if(ret == -1) {
-                    printf("read error\n");
-                    goto err;
-                } 
-                event = (struct inotify_event *)&buf[0];
-                while (ret > 0) {
-                    if (event->mask & IN_CREATE) {
-                        printf("file %s is created\n", event->name);
-                    }
-                    if (event->mask & IN_DELETE) {
-                        printf("file %s is deleted\n", event->name);
-                    }
-                    event = (struct inotify_event *)((char *)event + sizeof(struct inotify_event) + event->len);
-                    ret -= (sizeof(struct inotify_event) + event->len);
-		        }
-            }
-            else if(FD_ISSET(STDIN_FILENO, &fds)) {
-                memset(buf, 0, sizeof(buf));
-                ret = read(STDIN_FILENO, buf, sizeof(buf));
-                if(ret == -1) {
-                    printf("read error\n");
-                    goto err;
-                } 
-                printf("user input [%s]", buf);
-            }
-        }
-         
-               
-            
+	
+    	while (1) {
+		FD_SET(fd, &fds);
+		FD_SET(STDIN_FILENO, &fds);
+		timeout.tv_sec = 5;
+		timeout.tv_usec = 0;
+		ret = select(fd > STDIN_FILENO ? fd + 1 : STDIN_FILENO + 1, &fds,
+		    NULL, NULL, &timeout);
+		if(ret == -1) {
+		    printf("select error\n");
+		    goto err;
+		} else if(ret == 0) {
+		    printf("timeout expire !\n");
+		} else if(ret > 0) {
+		    if(FD_ISSET(fd, &fds)) {
+			memset(buf, 0, sizeof(buf));
+			ret = read(fd, buf, sizeof(buf));
+			if(ret == -1) {
+			    printf("read error\n");
+			    goto err;
+			} 
+			event = (struct inotify_event *)&buf[0];
+			while (ret > 0) {
+			    if (event->mask & IN_CREATE) {
+				printf("file %s is created\n", event->name);
+			    }
+			    if (event->mask & IN_DELETE) {
+				printf("file %s is deleted\n", event->name);
+			    }
+			    event = (struct inotify_event *)((char *)event + sizeof(struct inotify_event) + event->len);
+			    ret -= (sizeof(struct inotify_event) + event->len);
+				}
+		    }
+		    else if(FD_ISSET(STDIN_FILENO, &fds)) {
+			memset(buf, 0, sizeof(buf));
+			ret = read(STDIN_FILENO, buf, sizeof(buf));
+			if(ret == -1) {
+			    printf("read error\n");
+			    goto err;
+			} 
+			printf("user input [%s]", buf);
+		    }
+        	}
 	}
 	close(wd1);
 	close(wd2);
